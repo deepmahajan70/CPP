0161a5879d24503d7c32b80ab7d43f29deb6b529
Deepak Mahajan <deepmahajan70@gmail.com>
2024-02-28 20:15:57 +0530

    Add files via upload

--- ExampleStaticClassData.cpp
+++ ExampleStaticClassData.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 #include <string>
 class MovieCounter{
-    public: static int no_of_added_movies;
+    public: 
+            //Static Variable, will be shared amung all objects of the class
+            static int no_of_added_movies;
             MovieCounter(){
                 no_of_added_movies ++;
                 showCount();
             }
         void showCount(){
-        std::cout <<"Movies Added so far: "<< no_of_added_movies <<std::endl;
+            std::cout <<"Movies Added so far: "<< no_of_added_movies <<std::endl;
+        }
+            //static member function can be called without creating object of class 
+        static void showCountWithoutObject(){
+           std::cout <<"This function is called by class name itself...not by object"<<std::endl; 
         }
 };
 class Movie{
@@ -19,9 +25,19 @@ void addmovie(std::string mvname){
     m.mv_name = mvname;
     MovieCounter ic1;
 }
-int MovieCounter::no_of_added_movies = 0;
+int MovieCounter::no_of_added_movies; // because of this statement we can use 'no_of_added_movies' in main also
 int main(){
-    addmovie("Hum");
-    MovieCounter ic2;
-    return 0;
+    while(1){
+        std::string choice;
+        std::cout<<"Want to add movie ? Y / N" ;
+        std::cin>>choice;
+        if(choice =="y"){
+            addmovie("MyMovie");
+        }
+        else{
+            MovieCounter::showCountWithoutObject();//called by class itself...not by object
+            break;
+        }
+    }
+return 0;
 }

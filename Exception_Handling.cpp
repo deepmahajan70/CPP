c1b275fa9ad2d679faf5b65ffa015b43064cd89f
Deepak Mahajan <deepmahajan70@gmail.com>
2025-03-19 10:14:41 +0530

    Add files via upload

---
+++ Exception_Handling.cpp
@@ -0,0 +1,27 @@
+// Author: Deepak Mahajan
+// Objective: The code will demonstrate the use of - Exception Handling
+#include <iostream>
+using namespace std;
+
+class classname{
+
+};
+int main() {
+    int num,deno,result;
+    cout<<"Enter values for Numerator and Denominator"<<endl;
+    cin>>num>>deno;
+    try{
+        if(deno == 0){
+            throw(deno);
+        }
+        result = num / deno;
+    }
+    catch(int ex){
+        cout<<"Exception: Denomintor can not be 0"<<endl;
+        result = 0;
+    }
+    
+    cout<<"Result is : "<<result<<endl;
+
+    return 0;
+}
